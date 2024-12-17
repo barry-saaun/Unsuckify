d6ec318599899cda02c3b868e1754a616c96f6b8
Barry Saaun <barry@Barrys-MacBook-Pro.local>
2024-12-17 23:18:37 +0700

    feat: add protected route to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,16 +2,22 @@ import { cookies } from "next/headers"
 import { NextRequest, NextResponse } from "next/server"
 
 const publicRoute = ["/api/login", "/api/callback", "/login"]
+const protectedRoute = ["/dashboard"]
 
 export async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname
   const isPublicRoute = publicRoute.includes(path)
+  const isProtectedRoute = protectedRoute.includes(path)
 
   const cookiesStore = await cookies()
   const authenticated = cookiesStore.has("access_token")
 
   if (isPublicRoute && authenticated) {
     return NextResponse.redirect(new URL("/", req.nextUrl))
+  }
+
+  if (isProtectedRoute && !authenticated) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl))
   }
 
   return NextResponse.next()
