a5bc4f6722ff870fa6edf3f8411b588cd4c90856
Barry Saaun <barry@Barrys-MacBook-Pro.local>
2024-12-19 15:40:53 +0700

    Resolved conflict in DashboardPage

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,18 +5,22 @@ import { ListOfCurrentUsersPlaylistsResponse } from "spotify-api"
 
 export default function DashboardPage() {
   const { data, isLoading, error } =
-    useSpotify<ListOfCurrentUsersPlaylistsResponse>("/me/playlist")
+    useSpotify<ListOfCurrentUsersPlaylistsResponse>("/me/playlists")
 
   const playlistId = usePlaylistsIdStore((state) => state.playlistsId)
   const add_playlistId = usePlaylistsIdStore((state) => state.add_playlistId)
 
-  if (!data) return <div>No user data available</div> // Handle case with no user data
-
-  add_playlistId(data)
+  // useEffect(() => {
+  //   if (data) {
+  //     add_playlistId(data)
+  //   }
+  // }, [playlistId, add_playlistId, data])
 
   if (isLoading) return <div>Loading...</div> // Show loading state
 
   if (error) return <p>Error</p>
 
-  return <div>{JSON.stringify(playlistId)}</div>
+  return (
+    <div>{data?.items.map((item) => <div key={item.name}>{item.id}</div>)}</div>
+  )
 }
