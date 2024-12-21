408fa98b4fb3fcabe964c7ef26e3760ae473af6b
Barry Saaun <barry@Barrys-MacBook-Pro.local>
2024-12-21 14:08:39 +0700

    style: added loading skelton for playlist card

--- src/components/MyPlaylistsTabContent.tsx
+++ src/components/MyPlaylistsTabContent.tsx
@@ -1,25 +1,32 @@
 import useFetchPlaylistsMetadata from "@/hooks/useFetchPlaylistsMetadata"
 import PlaylistCard from "./PlaylistCard"
 import PlaylistImagePlaceholder from "../../public/playlist_image_placeholder.svg"
+import CardSkeleton from "./CardSkeleton"
 
 const MyPlaylistsTabContent = () => {
-  const { metadata: PlaylistMetadata } = useFetchPlaylistsMetadata()
+  const { metadata: PlaylistMetadata, isLoading } = useFetchPlaylistsMetadata()
+  const numberOfSkeleton = 6
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {PlaylistMetadata.items.map((item) => {
-        console.log(
-          item.url ? item.url : "https://placehold.co/600x600?text=No+Image"
-        )
-        return (
-          <PlaylistCard
-            key={item.id}
-            playlistImg={item.url || PlaylistImagePlaceholder}
-            owner={item.display_name}
-            playlistName={item.name}
-            numberOfTracks={item.total}
-          />
-        )
-      })}
+      {isLoading
+        ? Array.from({ length: numberOfSkeleton }).map((_, idx) => (
+            <CardSkeleton key={idx} />
+          ))
+        : PlaylistMetadata.items.map((item) => {
+            console.log(
+              item.url ? item.url : "https://placehold.co/600x600?text=No+Image"
+            )
+            return (
+              <PlaylistCard
+                key={item.id}
+                playlistImg={item.url || PlaylistImagePlaceholder}
+                owner={item.display_name}
+                playlistName={item.name}
+                numberOfTracks={item.total}
+                playlistId={item.id}
+              />
+            )
+          })}
     </div>
   )
 }
