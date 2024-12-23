41e181a558c8f7bb0b28299ec4aff3a42e7d3056
Barry Saaun <barry@Barrys-MacBook-Pro.local>
2024-12-23 16:02:44 +0700

    chore: added more protected routes to middleware for dev purposes for now

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,28 @@
 import { cookies } from "next/headers"
 import { NextRequest, NextResponse } from "next/server"
+import { match } from "path-to-regexp"
 
-const publicRoute = ["/api/login", "/api/callback", "/login"]
-const protectedRoute = ["/dashboard"]
+const publicRoutes = ["/api/login", "/api/callback", "/login"]
+const protectedRoutes = [
+  "/dashboard",
+  "/api/me",
+  "/api/me/playlist",
+  "/api/playlists/:playlist_id/tracks"
+]
+
+// Helper function to check if path matches any patterns
+const matchesRoute = (patterns: string[], path: string) =>
+  patterns.some((pattern) =>
+    match(pattern, { decode: decodeURIComponent })(path)
+  )
 
 export async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname
-  const isPublicRoute = publicRoute.includes(path)
-  const isProtectedRoute = protectedRoute.includes(path)
+
+  // const isPublicRoute = publicRoutes.includes(path)
+  // const isProtectedRoute = protectedRoutes.includes(path)
+  const isPublicRoute = matchesRoute(publicRoutes, path)
+  const isProtectedRoute = matchesRoute(protectedRoutes, path)
 
   const cookiesStore = await cookies()
   const authenticated = cookiesStore.has("access_token")
