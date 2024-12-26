df392fbab1e31d42c2c0f83853fb004398c158d4
Barry Saaun <barry@Barrys-MacBook-Pro.local>
2024-12-26 11:49:25 +0700

    style: restyled the Navbar to have sticky property and backdrop blur filter

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,20 +9,24 @@ import StarUs from "./StarUs"
 
 const NavBar: React.FC = () => {
   return (
-    <header className="flex flex-row justify-between items-center px-4 py-4">
-      <Link
-        href="/"
-        className="flex flex-row justify-center items-center gap-1"
-      >
-        <Disc2 className="text-primary" />
-        <span className="font-bold text-xl text-primary">UNSUCKify</span>
-      </Link>
-      <div className="flex flex-row gap-1 justify-center items-center">
-        <StarUs />
-        <ThemeToggle />
-        <AuthButton />
+    <div className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 max-w-screen-2xl items-center">
+        <div className="flex flex-1 justify-between items-center px-4 py-4">
+          <Link
+            href="/"
+            className="flex flex-row justify-center items-center gap-1"
+          >
+            <Disc2 className="text-primary" />
+            <span className="font-bold text-xl text-primary">UNSUCKify</span>
+          </Link>
+          <div className="flex flex-row gap-1 justify-center items-center">
+            <StarUs />
+            <ThemeToggle />
+            <AuthButton />
+          </div>
+        </div>
       </div>
-    </header>
+    </div>
   )
 }
 
